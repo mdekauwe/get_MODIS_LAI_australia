b88b3b84309c650a71964a0376a73c834ba85615
mdekauwe <mdekauwe@gmail.com>
2016-04-18 14:04:43 +1000

    Only write good LAI pixels

--- breakup_into_pixels_NSW.py
+++ breakup_into_pixels_NSW.py
@@ -27,11 +27,11 @@ def main():
     nrows = 681
     ndays = 366
     nvars = 2
-    row_st = 411
-    row_en = 461
+    row_st = 360
+    row_en = 560
     col_st = 570
     col_en = 840
-    """
+
     f = open("modis_climatology_splined.bin", "r")
     data = np.fromfile(f).reshape((nvars,ndays,nrows,ncols))
     f.close()
@@ -40,9 +40,11 @@ def main():
         for col in xrange(col_st, col_en):
             pixel = data[:,:,row,col]
 
-            pixel.tofile("pixels/%d_%d_LAI.bin" % (row, col))
+            # i.e. don't write the bad pixels out
+            if np.all(pixel[0,:] > -500.0):
+                pixel.tofile("pixels/%d_%d_LAI.bin" % (row, col))
+            
     """
-
     data = np.fromfile("pixels/%d_%d_LAI.bin" % (450, 717))
 
     data1 = data.reshape(nvars, ndays)[0,:]
@@ -53,7 +55,7 @@ def main():
     plt.plot(x1, data1, "r-")
     plt.plot(x2, data2[data2>-500.0], "ko")
     plt.show()
-
+    """
 
 if __name__ == "__main__":
 
